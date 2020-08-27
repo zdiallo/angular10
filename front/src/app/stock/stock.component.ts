70d861cc81d7a62b0ba57cad9131242b1416f85a
Zakaria DIALLO <diallozakaria@gmail.com>
2020-08-27 13:51:29 +0200

    added article interface

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { faRedoAlt, faPlus } from '@fortawesome/free-solid-svg-icons';
 
+import { Article } from 'src/app/interfaces/article';
+import { from } from 'rxjs';
+
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
-
   faRedoAlt = faRedoAlt;
   faPlus = faPlus;
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  articles: Article[] = [
+    { name: 'Tournevis', price: 2.4, qty: 140 },
+    { name: 'Tournevis cruciforme', price: 2.78, qty: 140 },
+    { name: 'Marteau', price: 2.4, qty: 140 },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
