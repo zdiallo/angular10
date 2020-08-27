69d98208a97f8d3a71ee74520dd34c8d0eb35212
Zakaria DIALLO <diallozakaria@gmail.com>
2020-08-27 15:18:38 +0200

    Add article process OK

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -6,12 +6,15 @@ import { Article } from 'src/app/interfaces/article';
   providedIn: 'root',
 })
 export class ArticleService {
-  constructor() {}
-
   articles: Article[] = [
     { name: 'Tournevis', price: 2.44, qty: 140 },
     { name: 'Tournevis cruciforme', price: 2.78, qty: 110 },
     { name: 'Marteau', price: 1.2, qty: 25 },
     { name: 'Pince', price: 4.21, qty: 13 },
   ];
+  constructor() {}
+
+  add(article: Article): void {
+    this.articles.push(article);
+  }
 }
--- front/src/app/stock/create/create.component.ts
+++ front/src/app/stock/create/create.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ArticleService } from 'src/app/services/article.service';
+import { Article } from 'src/app/interfaces/article';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
-
   f = new FormGroup({
     name: new FormControl('', [Validators.required]),
     price: new FormControl(10, [Validators.required]),
-    qty: new FormControl(1, [Validators.required])
+    qty: new FormControl(1, [Validators.required]),
   });
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private router: Router, private route: ActivatedRoute, private articleService: ArticleService) {}
+
+  ngOnInit(): void {}
+
+  submit(): void {
+    console.log(this.f.value);
+    this.articleService.add(this.f.value as Article);
+    this.router.navigate(['..'], { relativeTo: this.route });
   }
-
 }
