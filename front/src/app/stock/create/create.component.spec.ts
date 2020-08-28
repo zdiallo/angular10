cde4a281208410c94b7dbc5a472c24936e86384a
Zakaria DIALLO <diallozakaria@gmail.com>
2020-08-28 14:02:20 +0200

    NG test OK

--- front/src/app/stock/create/create.component.spec.ts
+++ front/src/app/stock/create/create.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateComponent } from './create.component';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CreateComponent', () => {
   let component: CreateComponent;
@@ -8,7 +10,8 @@ describe('CreateComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateComponent ]
+      declarations: [ CreateComponent ],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
   }));
